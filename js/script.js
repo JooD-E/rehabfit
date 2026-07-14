0d67046259e7dc79f05ef60ea42b37f03966a7ee
unknown <JooD-E>
2026-07-14 18:05:58 +0900

    bottom sheet, swiper 추가 0714

--- js/script.js
+++ js/script.js
@@ -36,4 +36,71 @@ document.addEventListener('DOMContentLoaded', function(){
             }
         });
     });
+
+
+    const markers = document.querySelectorAll('.pulse-marker');
+    const allSheets = document.querySelectorAll('.bottom-sheet');
+    const sheetDim = document.getElementById('sheet-dim');
+    const btnCloses = document.querySelectorAll('.btn-sheet-close');
+
+    const closeAllSheets = function(){
+        sheetDim.classList.remove('active');
+        allSheets.forEach(function(sheet){
+            sheet.classList.remove('active');
+        });
+    };
+
+    markers.forEach(function(marker){
+        marker.addEventListener('click', function(){
+            const targetId = this.getAttribute('data-target');
+            const targetSheet = document.getElementById(targetId);
+
+            if(targetSheet) {
+                closeAllSheets();
+
+                sheetDim.classList.add('active');
+                targetSheet.classList.add('active');
+            }
+        });
+    });
+
+    btnCloses.forEach(function(btn){
+        btn.addEventListener('click', closeAllSheets);
+    });
+    sheetDim.addEventListener('click', closeAllSheets);
+
+
+    const header = document.querySelector('.main-header');
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener('scroll', function(){
+        const currentScrollY = window.scrollY;
+
+        if(currentScrollY === 0) {
+            header.classList.remove('is-scrolled', 'is-hidden');
+        } else if ( currentScrollY > lastScrollY){
+            header.classList.add('is-hidden');
+            header.classList.remove('is-scrolled');
+        } else {
+            header.classList.remove('is-hidden');
+            header.classList.add('is-scrolled');
+        }
+        lastScrollY = currentScrollY;
+    })
+
+    const routineSwiper = new Swiper('.routine-swiper', {
+        slidesPerView: 1.2,
+        spaceBetween: 16,
+        grabCursor: true,
+        breakpoints: {
+            480: {
+                slidesPerView:1.5,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2.2,
+                spaceBetween: 24,
+            }
+        }
+    })
 });
